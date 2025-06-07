27977626c6bae7fc0ef0a9b7c26be56b346966fa
djmiro0 <mirosavljevicdjordje1@gmail.com>
2025-06-07 16:54:24 +0200

    vue cli, components created

---
+++ Vue/reaction-test/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" width="100px" height="100px">
+  <h1>Fast Reaction Game</h1>
+  <button class="button" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  components: {
+    Block
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.button {
+  margin: 50px 0;
+  background: rgb(26, 211, 171);
+  width: 100px;
+  padding: 10px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  border-radius: 30px;
+}
+</style>
---
+++ Vue/reaction-test/src/components/Block.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="block">
+    CLICK ME!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  props: ["delay"]
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.block {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  align-content: center;
+  background-color: aquamarine;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  border-radius: 10px;
+}
+</style>
